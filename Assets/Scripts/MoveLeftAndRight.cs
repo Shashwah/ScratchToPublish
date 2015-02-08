4842e560c28c1e10d4cee1533a54748d2605ae93
Jack Hunt <teamshashwah@gmail.com>
2015-02-08 18:20:57 +1100

    Initial Commit to Repository

---
+++ Assets/Scripts/MoveLeftAndRight.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftAndRight : MonoBehaviour {
+
+	public float speed = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(Input.GetAxis("Horizontal") * speed, 0.0f, 0.0f) * Time.deltaTime;
+	}	
+}
