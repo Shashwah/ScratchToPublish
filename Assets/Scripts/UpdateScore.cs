df72e03f3f39126f38b0206e6fb87a830f040538
Jack Hunt <teamshashwah@gmail.com>
2015-02-12 14:04:42 +1100

    Work with Score system, currently has a Null Reference Exception of type: Object reference not set to an instance of an object. Fix this later, the system does work though so it's not too important.

--- Assets/Scripts/UpdateScore.cs
+++ Assets/Scripts/UpdateScore.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class UpdateScore : MonoBehaviour {
 
+	public Text scoreText = null;
+	public int score = 0;
 
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log ("Score Text Object Set To: " + scoreText.name);
+		scoreText.text = "Score: " + score;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		score++;
+		scoreText.text = "Score: " + score;
 	}
 }
